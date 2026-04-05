c11547d5e0afa722c98a414c7dae22f534d2f403
cesarWrites <cesarwrites24@gmail.com>
2026-04-06 01:25:17 +0300

    adsense fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,14 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4833804033566999"
-     crossOrigin="anonymous"></script> */}
-       <Script
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4833804033566999"
+     crossOrigin="anonymous"></script>
+       {/* <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4833804033566999"
           crossOrigin="anonymous"
           strategy="afterInteractive"
-        />
+        /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
